4056a90358eaa6f888ace60d964e239f7ee18de0
hauntph07061 <86589665+hauntph07061@users.noreply.github.com>
2024-11-25 12:42:01 +0700

    commit

---
+++ profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
@@ -0,0 +1,42 @@
+package com.devteria.profile.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Node("user_profile")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    String id;
+
+    @Property("userId")
+    String userId;
+
+    String firstName;
+
+    LocalDate dob;
+
+    String lastName;
+
+    String city;
+}
